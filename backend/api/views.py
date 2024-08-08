a932f8a60e26febc0bfd6f59e87b49b88ee23bac
ta-muu <122217578+ta-muu@users.noreply.github.com>
2024-08-08 23:23:12 +0900

    feature: UserModelのviewを追加

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from reset_framework import iewsets, filters
+from .models import User
+from .serializer import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
 
 # Create your views here.
