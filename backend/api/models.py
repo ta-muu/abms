d4e9da9e785c9316aab6ccc409dc6ee9e26c893a
ta-muu <122217578+ta-muu@users.noreply.github.com>
2024-08-08 23:16:20 +0900

    feature: UserModelを追加

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	user_name = models.CharField(max_length=50)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	deleted_at = models.DateTimeField(blank=True, null=True)
+	mail_adress = models.EmailField()
